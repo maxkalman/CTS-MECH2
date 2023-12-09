f2b1cdac661b500427966c1b450ccf7526625c34
Max Kalman <maxkalman@yahoo.com>
2023-12-09 14:41:46 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entries => {
+   entries.forEach(entry => {
+      if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+      }
+   });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+
+hiddenElements.forEach(el => observer.observe(el));
+hiddenElements2.forEach(el => observer.observe(el));
